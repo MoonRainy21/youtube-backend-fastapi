e829cd102595d8d781ce479d3fead93f8227ec92
MoonRainy21 <shmoon323@gmail.com>
2023-09-05 19:33:40 +0900

    add /video and /playlist

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from yt_dlp import YoutubeDL
 from fastapi import FastAPI, responses, Header, HTTPException
 import pyotp
 from dotenv import load_dotenv, dotenv_values
+import zipfile
 
 load_dotenv()
 config = dotenv_values('.env')
@@ -18,10 +19,10 @@ def check_video_id(video_id: str):
         return False
     return True
 
-async def get_yt_filename(video_id: str, ydl: YoutubeDL):
+async def get_yt_filename(video_id: str, ydl: YoutubeDL, ext: str):
     info = ydl.extract_info(f'https://www.youtube.com/watch?v={video_id}', download=False)
     file_name = ydl.prepare_filename(info)
-    file_name = (file_name.split('.')[:-1] + ['.mp3'])
+    file_name = (file_name.split('.')[:-1] + [ext])
     file_name = ''.join(file_name)
     return file_name
 
@@ -48,12 +49,73 @@ async def get_yt(id: str, otp: Annotated[str, Header()]):
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
-        }],
-        'ffmpeg-location': 'ffmpeg'
+        }]
     }
 
     with YoutubeDL(ydl_opts) as ydl:
-        file_name = await get_yt_filename(id, ydl)
+        file_name = await get_yt_filename(id, ydl, 'mp3')
         print(file_name)
         ydl.download([f'https://www.youtube.com/watch?v={id}'])
     return responses.FileResponse(file_name, media_type='application/octet-stream', filename=file_name)
+
+@app.get("/yt/video")
+async def get_yt_video(id: str, otp: Annotated[str, Header()]):
+    password = config['PASSWORD']
+    totp = pyotp.TOTP(password)
+    if totp.verify(otp):
+        print('OTP Verified')
+    else:
+        raise HTTPException(status_code=401, detail='Invalid OTP')
+    if check_video_id(id) is False:
+        raise HTTPException(status_code=400, detail='Invalid Video ID')
+    if id.startswith('https://www.youtube.com/watch?v='):
+        id = id.split('https://www.youtube.com/watch?v=')[1]
+
+    ydl_opts = {
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+        'outtmpl': 'downloads/%(title)s-%(format_id)s.mp4'
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        file_name = await get_yt_filename(id, ydl, 'mp4')
+        print(file_name)
+        ydl.download([f'https://www.youtube.com/watch?v={id}'])
+    return responses.FileResponse(file_name, media_type='application/octet-stream', filename=file_name)
+
+@app.get("/yt/playlist-audio")
+async def get_yt_playlist(id: str, otp: Annotated[str, Header()]):
+    password = config['PASSWORD']
+    totp = pyotp.TOTP(password)
+    if totp.verify(otp):
+        print('OTP Verified')
+    else:
+        raise HTTPException(status_code=401, detail='Invalid OTP')
+    if check_video_id(id) is False:
+        raise HTTPException(status_code=400, detail='Invalid Video ID')
+    if id.startswith('https://www.youtube.com/watch?v='):
+        id = id.split('https://www.youtube.com/watch?v=')[1]
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': 'downloads/%(title)s-%(format_id)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+        }]
+    }
+
+    files = []
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(f'https://www.youtube.com/playlist?list={id}', download=False)
+        title = info['title']
+        for video in info['entries']:
+            file_name = await get_yt_filename(video['id'], ydl, 'mp3')
+            files.append(file_name)
+            ydl.download([f'https://www.youtube.com/watch?v={video["id"]}'])
+    
+    # zip files
+    zip_file_name = f'{title}.zip'
+    with zipfile.ZipFile(zip_file_name, 'w') as zip:
+        for file in files:
+            zip.write(file)
+    return responses.FileResponse(zip_file_name, media_type='application/octet-stream', filename=zip_file_name)
