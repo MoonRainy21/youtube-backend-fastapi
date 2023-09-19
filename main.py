c21310139061695642facf89033e4354251500c6
MoonRainy21 <shmoon323@gmail.com>
2023-09-19 09:30:18 +0000

    make a title endpoint

--- main.py
+++ main.py
@@ -40,6 +40,31 @@ async def get_yt_filename(video_id: str, ydl: YoutubeDL, ext: str):
 def read_root():
     return {"Hello": "World"} 
 
+@app.get("/yt/title")
+async def get_yt_title(id: str, otp: Annotated[str, Header()]):
+    password = config['PASSWORD']
+    totp = pyotp.TOTP(password)
+    if totp.verify(otp):
+        print('OTP Verified')
+    else:
+        raise HTTPException(status_code=401, detail='Invalid OTP')
+    if check_video_id(id) is False:
+        raise HTTPException(status_code=400, detail='Invalid Video ID')
+    if id.startswith('https://www.youtube.com/watch?v='):
+        id = id.split('https://www.youtube.com/watch?v=')[1]
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': 'downloads/%(title)s-%(format_id)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+        }]
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(f'https://www.youtube.com/watch?v={id}', download=False)
+        return info['title']
+
 @app.get("/yt/audio")
 async def get_yt(id: str, otp: Annotated[str, Header()]=None):
     password = config['PASSWORD']
@@ -55,7 +80,8 @@ async def get_yt(id: str, otp: Annotated[str, Header()]=None):
 
     # if the file already exists, return it
     if os.path.exists(f'downloads/{id}-audio.mp3'):
-        return responses.FileResponse(f'downloads/{id}-audio.mp3', media_type='application/octet-stream', filename=f'{id}-audio.mp3')
+        return responses.FileResponse(f'downloads/{id}-audio.mp3', media_type='application/octet-stream', filename=f'{id}-audio.mp3',
+                                      headers={'Access-Control-Expose-Headers': 'Content-Disposition'})
 
     ydl_opts = {
         'format': 'bestaudio/best',
@@ -73,7 +99,8 @@ async def get_yt(id: str, otp: Annotated[str, Header()]=None):
     # rename the downloaded file
     new_file_name = 'downloads/'+id+'-audio'+'.mp3'
     os.rename(file_name, new_file_name)
-    return responses.FileResponse(new_file_name, media_type='application/octet-stream', filename=new_file_name)
+    return responses.FileResponse(new_file_name, media_type='application/octet-stream', filename=file_name,
+                                  headers={'Access-Control-Expose-Headers': 'Content-Disposition'})
 
 @app.get("/yt/video")
 async def get_yt_video(id: str, otp: Annotated[str, Header()]):
@@ -96,7 +123,10 @@ async def get_yt_video(id: str, otp: Annotated[str, Header()]):
     with YoutubeDL(ydl_opts) as ydl:
         file_name = await get_yt_filename(id, ydl, 'mp4')
         print(file_name)
-        ydl.download([f'https://www.youtube.com/watch?v={id}'])
+        try:
+            ydl.download([f'https://www.youtube.com/watch?v={id}'])
+        except:
+            raise HTTPException(status_code=500, detail='Internal Server Error')
     return responses.FileResponse(file_name, media_type='application/octet-stream', filename=file_name)
 
 @app.get("/yt/playlist-audio")
